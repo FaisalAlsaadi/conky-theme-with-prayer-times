4d304dd9e07fda5a5ccdb5b7eb13265272191d02
TheDucky <dakshkuldeep12@gmail.com>
2023-11-27 21:12:42 +0530

    going for a simpler design

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
  
 # DATA
-playing=''
-pause=''
-status=` dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | tail -1 | cut -d "\"" -f2`
+playing=' '
+pause=' '
+# status=` dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | tail -1 | cut -d "\"" -f2`
 # ---
 
 
@@ -16,16 +16,16 @@ symDisplay() {
     fi
 }
 
-statStr() {
-    echo $status
-}
+# statStr() {
+# 	 echo $status
+# }
 # ---
 
 
 # ACT_TO_ARGS
-if [ "$1" = "--stat_str" ]; then
-    statStr
-elif [ "$1" = "--sym_display" ]; then
+# if [ "$1" = "--stat_str" ]; then
+    # statStr
+if [ "$1" = "--sym_display" ]; then
     symDisplay
 else
     echo "invalid args: $1"
