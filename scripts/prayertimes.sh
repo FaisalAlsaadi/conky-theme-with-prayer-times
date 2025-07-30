975d5c25a1a98774bf35669af288644f92ecfe8b
FaisalAlsaadi <128548030+FaisalAlsaadi@users.noreply.github.com>
2025-07-30 16:44:47 +0300

    Add files via upload

---
+++ scripts/prayertimes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+json=$(curl -sL "https://api.aladhan.com/v1/timingsByCity?city=Doha&country=Qatar&method=10")
+
+if [[ -z "$json" ]]; then
+    echo "Failed to fetch prayer times."
+    exit 1
+fi
+
+fajr=$(echo "$json" | jq -r '.data.timings.Fajr')
+dhuhr=$(echo "$json" | jq -r '.data.timings.Dhuhr')
+asr=$(echo "$json" | jq -r '.data.timings.Asr')
+maghrib=$(echo "$json" | jq -r '.data.timings.Maghrib')
+isha=$(echo "$json" | jq -r '.data.timings.Isha')
+
+printf "%-9s %s\n" "     Fajr:" "   $fajr"
+printf "%-9s %s\n" "	Dhuhr:" "$dhuhr"
+printf "%-9s %s\n" "	Asr:" "$asr"
+printf "%-9s %s\n" "	Maghrib:" "$maghrib"
+printf "%-9s %s\n" "	Isha:" "$isha"
+
---
+++ scripts/weather.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API_KEY="ffd429e683762746ff8e45006cb1dde6"
+CITY="Doha"
+COUNTRY="QA"
+
+response=$(curl -s "https://api.openweathermap.org/data/2.5/weather?q=${CITY},${COUNTRY}&units=metric&appid=${API_KEY}")
+if [[ $? -ne 0 || -z "$response" ]]; then
+  echo "Failed to fetch weather"
+  exit 1
+fi
+
+temp=$(echo "$response" | jq '.main.temp' | awk '{printf("%.1f°C", $1)}')
+condition=$(echo "$response" | jq -r '.weather[0].description' | sed 's/\b\(.\)/\u\1/g')
+
+echo "${temp}, ${condition}"
+
