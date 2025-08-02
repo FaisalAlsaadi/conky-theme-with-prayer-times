1db131765399c7c9de6fae1d69b46dab58fd284f
FaisalAlsaadi <128548030+FaisalAlsaadi@users.noreply.github.com>
2025-08-02 11:28:11 +0300

    Update prayertimes.sh
    spacing

--- scripts/prayertimes.sh
+++ scripts/prayertimes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ENTER CITY AND COUNTY AND MEASUREMENT METHOD BELOW ACCORDING TO ALADHAN API https://aladhan.com/prayer-times-api
+
 json=$(curl -sL "https://api.aladhan.com/v1/timingsByCity?city=Doha&country=Qatar&method=10")
 
 if [[ -z "$json" ]]; then
@@ -13,9 +13,9 @@ asr=$(echo "$json" | jq -r '.data.timings.Asr')
 maghrib=$(echo "$json" | jq -r '.data.timings.Maghrib')
 isha=$(echo "$json" | jq -r '.data.timings.Isha')
 
-printf "%-9s %s\n" "     Fajr:" "   $fajr"
-printf "%-9s %s\n" "	Dhuhr:" "$dhuhr"
-printf "%-9s %s\n" "	Asr:" "$asr"
-printf "%-9s %s\n" "	Maghrib:" "$maghrib"
-printf "%-9s %s\n" "	Isha:" "$isha"
-
+# Use fixed spacing with dots for better alignment visibility
+echo "$fajr"
+echo "$dhuhr"
+echo "$asr"
+echo "$maghrib"
+echo "$isha"
